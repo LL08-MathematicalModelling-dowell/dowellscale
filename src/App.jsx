ec72657b8fa15716ffafd5135126dc8f7e9d2097
Manish <mdashsharma95@gmail.com>
2024-05-17 12:38:29 +0530

    Dowell Scale for CollegeScaleReport

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import NpsLiteScale from "./pages/NpsLiteScale";
 import HomePage from "./pages/HomePage";
 import ReportPage from "./pages/ReportPage";
+import DowellScaleForCollege from "./pages/DowellScaleForCollege";
+import DowellScaleForCollegeReport from "./pages/CollegeScaleReport";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/dowellscale" element={<HomePage />} />
         <Route path="/dowellscale/npslitescale" Component={NpsLiteScale} />
         <Route path="/dowellscale/report" Component={ReportPage} />
+        <Route path="/dowellscale/dowellscaleforcollege" Component={DowellScaleForCollege} />
+        <Route path="/dowellscale/dowellscalecollegereport" Component={DowellScaleForCollegeReport} />
       </Routes>
     </>
   );
