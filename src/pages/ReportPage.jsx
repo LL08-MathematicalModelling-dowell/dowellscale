09b882b5b4806544348a0e7e541947d8c9cb9694
Ayoola <ayo.oloyede16@gmail.com>
2024-05-17 10:56:01 +0100

    Add daily insight chart and all channel selection to reports page

--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Select,
   MenuItem,
@@ -9,6 +9,25 @@ import {
   Box,
 } from "@mui/material";
 import axios from "axios";
+import { 
+  Chart as ChartJS, 
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const instanceNames = {
   instance_1: "Aspirational",
@@ -18,8 +37,71 @@ const instanceNames = {
   instance_5: "Valuable",
 };
 
+const allChannelsNameTag = 'channel_all_x';
+
 const channelNames = {
+  [`${allChannelsNameTag}`]: "All Channels",
   channel_1: "Social media post",
+};
+
+const options = {
+  indexAxis: 'y',
+  elements: {
+    bar: {
+      borderWidth: 0,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Responses Insights by Day',
+    },
+  },
+};
+
+const extractLabelsAndDatasetsInfo = (data=[]) => {
+  const labelsForCharts = [
+    ...new Set(
+      data
+        .map(
+          item => 
+            item?.dowell_time?.current_time && new Date(item?.dowell_time?.current_time).toDateString()
+      )
+    )
+  ];
+  
+  const datasetsForCharts = {
+    yesData: [],
+    noData: [],
+    maybeData: []
+  };
+
+  labelsForCharts.forEach(item => {
+    const matchingData = data
+      .filter(dataItem => dataItem?.dowell_time?.current_time && new Date(dataItem?.dowell_time?.current_time).toDateString() === item);
+
+    datasetsForCharts.noData.push(matchingData.filter(data => data?.score === 0).length);
+    datasetsForCharts.maybeData.push(matchingData.filter(data => data?.score === 1).length);
+    datasetsForCharts.yesData.push(matchingData.filter(data => data?.score === 2).length);
+  });
+
+  const totalItems = datasetsForCharts.yesData.reduce((a, b) => a + b, 0) + datasetsForCharts.noData.reduce((a, b) => a + b, 0) + datasetsForCharts.maybeData.reduce((a, b) => a + b, 0)
+
+  return {
+    labels: labelsForCharts,
+    datasetsInfo: datasetsForCharts,
+    totalItems,
+  }
+}
+
+const initialScoreData = {
+  No: { count: 0, percentage: 0 },
+  Maybe: { count: 0, percentage: 0 },
+  Yes: { count: 0, percentage: 0 },
 };
 
 const App = () => {
@@ -27,18 +109,121 @@ const App = () => {
   const [instances, setInstances] = useState([]);
   const [selectedChannel, setSelectedChannel] = useState("");
   const [selectedInstance, setSelectedInstance] = useState("");
-  const [scores, setScores] = useState({
-    No: { count: 0, percentage: 0 },
-    Maybe: { count: 0, percentage: 0 },
-    Yes: { count: 0, percentage: 0 },
-  });
+  const [scores, setScores] = useState(initialScoreData);
   const [totalCount, setTotalCount] = useState(0);
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [dateDataForChart, setDateDataForChart] = useState({
+    labels: [],
+    datasets: [],
+  });
+  const [displayDataForAllSelection, setDisplayDataForAllSelection] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (selectedChannel.length < 1 || selectedInstance.length < 1) return;
+
+    const { labels, datasetsInfo, totalItems } = extractLabelsAndDatasetsInfo(
+      data.filter(item => item.channel_name === selectedChannel && item.instance_name === selectedInstance)
+    );
+    
+    // console.log(labels);
+    // console.log((datasetsInfo));
+    // console.log('total -> ', totalItems)
+
+    setDateDataForChart({
+      labels: labels,
+      datasets: [
+        {
+          label: 'No',
+          data: datasetsInfo.noData,
+          borderColor: 'red',
+          backgroundColor: 'red',
+        },
+        {
+          label: 'Yes',
+          data: datasetsInfo.yesData,
+          borderColor: 'green',
+          backgroundColor: 'green',
+        },
+        {
+          label: 'Maybe',
+          data: datasetsInfo.maybeData,
+          borderColor: 'yellow',
+          backgroundColor: 'yellow',
+        },
+      ]
+    })
+
+  }, [selectedChannel, selectedInstance, data])
+
+  useEffect(() => {
+    if (selectedChannel !== allChannelsNameTag) return setDisplayDataForAllSelection([]);
+
+    const allData = instances.map(instance => {
+      const dataForInstance = data.filter(item => item.instance_name === instance);
+      const scoreCounts = {
+        No: {
+          count: 0,
+          percentage: 0,
+        }, 
+        Maybe: {
+          count: 0,
+          percentage: 0,
+        }, 
+        Yes: {
+          count: 0,
+          percentage: 0,
+        },
+      };
+
+      const { labels, datasetsInfo, totalItems } = extractLabelsAndDatasetsInfo(dataForInstance);
+
+      scoreCounts.No.count = datasetsInfo.noData.length;
+      scoreCounts.No.percentage = ( datasetsInfo.noData.reduce((a, b) => a + b, 0) / totalItems ) * 100;
+      
+      scoreCounts.Maybe.count = datasetsInfo.maybeData.length;
+      scoreCounts.Maybe.percentage = ( datasetsInfo.maybeData.reduce((a, b) => a + b, 0) / totalItems ) * 100;
+
+      scoreCounts.Yes.count = datasetsInfo.yesData.length;
+      scoreCounts.Yes.percentage = ( datasetsInfo.yesData.reduce((a, b) => a + b, 0) / totalItems ) * 100;
+
+      return {
+        instanceName: instance,
+        totalResponses: dataForInstance.length,
+        scoreCounts,
+        chartData: {
+          labels: labels,
+          datasets: [
+            {
+              label: 'No',
+              data: datasetsInfo.noData,
+              borderColor: 'red',
+              backgroundColor: 'red',
+            },
+            {
+              label: 'Yes',
+              data: datasetsInfo.yesData,
+              borderColor: 'green',
+              backgroundColor: 'green',
+            },
+            {
+              label: 'Maybe',
+              data: datasetsInfo.maybeData,
+              borderColor: 'yellow',
+              backgroundColor: 'yellow',
+            },
+          ]
+        },
+      }
+    })
+
+    setDisplayDataForAllSelection(allData);
+
+  }, [selectedChannel, data, instances])
 
   const fetchData = async () => {
     try {
@@ -54,7 +239,7 @@ const App = () => {
         new Set(data.map((item) => item.instance_name))
       );
 
-      setChannels(uniqueChannels);
+      setChannels([allChannelsNameTag, ...uniqueChannels]);
       setInstances(uniqueInstances);
       setData(data);
       setLoading(false);
@@ -66,6 +251,12 @@ const App = () => {
 
   const handleChannelSelect = (event) => {
     setSelectedChannel(event.target.value);
+
+    if (event.target.value === allChannelsNameTag) {
+      setSelectedInstance("");
+      setScores(initialScoreData);
+      setTotalCount(0);
+    }
   };
 
   const handleInstanceSelect = (event) => {
@@ -152,6 +343,7 @@ const App = () => {
             onChange={handleInstanceSelect}
             displayEmpty
             fullWidth
+            disabled={selectedChannel === allChannelsNameTag}
           >
             <MenuItem value="" disabled>
               Select Instance
@@ -164,45 +356,127 @@ const App = () => {
           </Select>
         </Grid>
       </Grid>
-      <Typography
-        variant="body1"
-        align="center"
-        gutterBottom
-        style={{ marginTop: "16px" }}
-      >
-        Total Responses: {totalCount}
-      </Typography>
-      <Typography variant="body1" align="center" gutterBottom>
-        Scores:
-      </Typography>
-      <Grid container spacing={3} alignItems="center" justifyContent="center">
-        {Object.entries(scores).map(([score, data]) => (
-          <Grid item xs={12} sm={4} key={score}>
-            <Box textAlign="center">
-              <Typography variant="subtitle1" gutterBottom>
-                {`${score}: ${data.count} (${data.percentage.toFixed(2)}%)`}
-              </Typography>
-              <LinearProgress
-                variant="determinate"
-                value={data.percentage}
-                sx={{
-                  height: "20px",
-                  borderRadius: "10px",
-                  "& .MuiLinearProgress-bar": {
-                    borderRadius: "10px",
-                    backgroundColor:
-                      score === "No"
-                        ? "red"
-                        : score === "Maybe"
-                        ? "yellow"
-                        : "green",
-                  },
-                }}
-              />
-            </Box>
+      {
+        selectedChannel === allChannelsNameTag ? <>
+          {
+            React.Children.toArray(displayDataForAllSelection.map((item, index) => {
+              return <>
+                <Typography
+                  variant="h6"
+                  align="left"
+                  style={{ marginTop: "32px", marginBottom: "10px" }}
+                >
+                  {index + 1}. {instanceNames[item?.instanceName]}
+                </Typography>
+                
+                <Typography
+                  variant="body1"
+                  align="center"
+                  gutterBottom
+                  style={{ marginTop: "16px" }}
+                >
+                  Total Responses: {item?.totalResponses}
+                </Typography>
+                <Typography variant="body1" align="center" gutterBottom>
+                  Scores:
+                </Typography>
+                <Grid container spacing={3} alignItems="center" justifyContent="center">
+                  {Object.entries(item?.scoreCounts).map(([score, data]) => (
+                    <Grid item xs={12} sm={4} key={score}>
+                      <Box textAlign="center">
+                        <Typography variant="subtitle1" gutterBottom>
+                          {`${score}: ${data.count} (${data.percentage.toFixed(2)}%)`}
+                        </Typography>
+                        <LinearProgress
+                          variant="determinate"
+                          value={data.percentage}
+                          sx={{
+                            height: "20px",
+                            borderRadius: "10px",
+                            "& .MuiLinearProgress-bar": {
+                              borderRadius: "10px",
+                              backgroundColor:
+                                score === "No"
+                                  ? "red"
+                                  : score === "Maybe"
+                                  ? "yellow"
+                                  : "green",
+                            },
+                          }}
+                        />
+                      </Box>
+                    </Grid>
+                  ))}
+                </Grid>
+                <>
+                  <br />
+                  <br />
+
+                  <Bar 
+                    options={options} 
+                    data={item?.chartData} 
+                  />;
+                </>
+
+                <br />
+                <br />
+              </>
+            }))
+          }
+        </> : <>
+          <Typography
+            variant="body1"
+            align="center"
+            gutterBottom
+            style={{ marginTop: "16px" }}
+          >
+            Total Responses: {totalCount}
+          </Typography>
+          <Typography variant="body1" align="center" gutterBottom>
+            Scores:
+          </Typography>
+          <Grid container spacing={3} alignItems="center" justifyContent="center">
+            {Object.entries(scores).map(([score, data]) => (
+              <Grid item xs={12} sm={4} key={score}>
+                <Box textAlign="center">
+                  <Typography variant="subtitle1" gutterBottom>
+                    {`${score}: ${data.count} (${data.percentage.toFixed(2)}%)`}
+                  </Typography>
+                  <LinearProgress
+                    variant="determinate"
+                    value={data.percentage}
+                    sx={{
+                      height: "20px",
+                      borderRadius: "10px",
+                      "& .MuiLinearProgress-bar": {
+                        borderRadius: "10px",
+                        backgroundColor:
+                          score === "No"
+                            ? "red"
+                            : score === "Maybe"
+                            ? "yellow"
+                            : "green",
+                      },
+                    }}
+                  />
+                </Box>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+          {
+            selectedChannel.length < 1 || selectedInstance.length < 1 ? <></> :
+              <>
+                <br />
+                <br />
+
+                <Bar 
+                  options={options} 
+                  data={dateDataForChart} 
+                />;
+            </>
+          } 
+        </>
+      }
     </Box>
   );
 };
