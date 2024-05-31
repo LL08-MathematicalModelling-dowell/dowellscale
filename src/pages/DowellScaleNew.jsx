defded1a9f664076e536b31db7e4f8e58e51ba9f
Pranai-1 <reddypranai2017@gmail.com>
2024-05-31 20:07:08 +0530

    added custom styling and removed console logs

--- src/pages/DowellScaleNew.jsx
+++ src/pages/DowellScaleNew.jsx
@@ -6,7 +6,7 @@ import brainImage from "../assets/images/brain.png";
 import discussionImage from "../assets/images/discussion.png";
 import developmentImage from "../assets/images/development.png";
 import axios from "axios";
-import zIndex from "@mui/material/styles/zIndex";
+
 
 
 const questions = [
@@ -39,7 +39,7 @@ export default function DowellScaleForCollege() {
     };
 
     const handleSubmission = async (index, i) => {
-        console.log(answered,index)
+
         if(answered!=index+1){
         setAnswered(index+1)
      
@@ -67,12 +67,47 @@ export default function DowellScaleForCollege() {
                 <p>Bengaluru</p>
             </div>
             <p className="text-[12px] md:text-[16px] text-black flex justify-center items-center my-5 font-bold">Which stage are you in?</p>
-            <div className="flex flex-col justify-center items-center  mt-3 gap-6 ml-auto w-[80%] sm:gap-2">
+            <style>
+                {`
+                .custom-div {
+                   width:95%;
+                   margin-left:10px;
+                }
+                @media (min-width: 400px) {
+                    .custom-div {
+                     margin-left: auto;
+                     width: 80%;
+                    }
+                  }
+                `}
+      </style>
+            <div className="flex flex-col justify-center items-center  mt-3 gap-6  sm:gap-2 custom-div">
                 {questions.map((question, index) => (
                     <div key={index} className="flex flex-col justify-center items-center w-full mb-4 ">
-                        <div className="flex justify-start items-center sm:gap-2 lg:gap-8 md:gap-4 gap-1 w-[100%] sm:px-4">
-   
-                          <img src={images[index]} alt="image" className="lg:w-[100px] sm:w-[70px] w-[30px]"/>
+                        <div className="flex justify-start items-center sm:gap-2 lg:gap-8 md:gap-4 gap-[2px] w-[100%] sm:px-4">
+                        <style>
+                {`
+                .custom-img {
+                   width:20px;
+                }
+                @media (min-width: 338px) {
+                    .custom-img {
+                     width: 50px;
+                    }
+                  }
+                  @media (min-width: 640px) {
+                    .custom-img {
+                     width: 70px;
+                    }
+                  }
+                  @media (min-width:1024px) {
+                    .custom-img {
+                     width: 100px;
+                    }
+                  }
+                `}
+      </style>
+                          <img src={images[index]} alt="image" className="custom-img "/>
 
                             <div className="flex flex-col gap-2 min-w-[80px] md:min-w-[150px] lg:min-w-[200px] w-1/4 justify-center items-center">
                             <div className="lg:text-lg font-semibold md:text-[14px] text-[10px]">{question}</div>
@@ -90,7 +125,7 @@ export default function DowellScaleForCollege() {
                                 {clicked[index] && <span className="p-2">&#10003;</span>}
                             </button>
                             {clicked[index]  && (
-                            <div className="flex justify-center items-center gap-2  mt-2">
+                            <div className="flex justify-center items-center gap-[2px] sm:gap-2  mt-2">
                                 {buttons.map((data,i)=>(
                                      <button key={i} onClick={()=>{
                                         handleSubmission(index,i)
